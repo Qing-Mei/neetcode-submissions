419b0d23b2d91a81d47fdc2bb3c42ded8717715b
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-08-20 01:32:52 -0400

    Add: design-twitter-feed - submission-0

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-0.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+import heapq
+
+class Twitter:
+
+    def __init__(self):
+        self.posts = defaultdict(list)
+        self.following = defaultdict(set)
+        self.time = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.posts[userId].append((self.time, tweetId))
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        max_heap = []
+
+        users = self.following[userId] | {userId}
+        for uid in users:
+            if self.posts[uid]:
+                i = len(self.posts[uid]) - 1
+                time, tweetId = self.posts[uid][i]
+                heapq.heappush(max_heap, (-time, tweetId, uid, i))
+
+        feed = []
+        while max_heap and len(feed) < 10:
+            neg_time, tweetId, uid, i = heapq.heappop(max_heap)
+            feed.append(tweetId)
+
+            if i > 0:
+                time, tweetId = self.posts[uid][i - 1]
+                heapq.heappush(max_heap, (-time, tweetId, uid, i - 1))
+        
+        return feed
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.following:
+            return
+        self.following[followerId].discard(followeeId)
