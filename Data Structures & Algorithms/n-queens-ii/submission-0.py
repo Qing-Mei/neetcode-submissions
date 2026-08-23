6e55c5c77856f5bfd7716acd0216353e91307582
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-08-22 21:26:39 -0400

    Add: n-queens-ii - submission-0

---
+++ Data Structures & Algorithms/n-queens-ii/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        cnt = 0
+
+        col = 0
+        diag1 = 0
+        diag2 = 0
+
+        def dfs(r):
+            nonlocal col, diag1, diag2, cnt, n
+
+            if r == n:
+                cnt += 1
+                return
+            
+            for c in range(n):
+                d1 = r - c + n - 1
+                d2 = r + c
+                
+                if ((1 << c) & col) or ((1 << d1) & diag1) or ((1 << d2) & diag2):
+                    continue
+                
+                col |= (1 << c)
+                diag1 |= (1 << d1)
+                diag2 |= (1 << d2)
+                dfs(r + 1)
+
+                col ^= (1 << c)
+                diag1 ^= (1 << d1)
+                diag2 ^= (1 << d2)
+
+        dfs(0)
+
+        return cnt
