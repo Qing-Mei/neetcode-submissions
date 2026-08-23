0ae2afcbf3967684c6d37e331ac158597a888c74
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-08-22 22:59:58 -0400

    Add: car-pooling - submission-5

---
+++ Data Structures & Algorithms/car-pooling/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        events = []
+
+        for passenger, start, end in trips:
+            events.append((start, passenger))
+            events.append((end, -passenger))
+        
+        events.sort()
+        curr = 0
+
+        for _, passenger in events:
+            curr += passenger
+            if curr > capacity:
+                return False
+        
+        return True
+        
