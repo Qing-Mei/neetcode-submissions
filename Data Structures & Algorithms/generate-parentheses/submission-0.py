360ee6e06b2cad5d7df8d27088f9ba361746760b
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-08-26 21:33:54 -0400

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        path = []
+
+        def dfs(l, r):
+            if l == n and r == n:
+                res.append("".join(path))
+                return
+            
+            if l < n:
+                path.append("(")
+                dfs(l + 1, r)
+                path.pop()
+            
+            if r < l:
+                path.append(")")
+                dfs(l, r + 1)
+                path.pop()
+        
+        dfs(0, 0)
+        return res
