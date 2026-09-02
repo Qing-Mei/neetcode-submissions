b2c4c7e0bdfdd06195f77e3a59c235d79a2cb8c0
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-09-01 20:57:39 -0400

    Add: open-the-lock - submission-0

---
+++ Data Structures & Algorithms/open-the-lock/submission-0.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        deadends_set = set(deadends)
+
+        if "0000" in deadends_set:
+            return -1
+
+        q = deque(["0000"])
+        visited = {"0000"}
+        steps = 0
+
+        while q:
+            sz = len(q)
+
+            for _ in range(sz):
+                curr = q.popleft()
+
+                if curr == target:
+                    return steps
+
+                for i in range(4):
+                    digit = int(curr[i])
+
+                    for next_digit in [(digit + 1) % 10, (digit - 1) % 10]:
+                        nxt = curr[:i] + str(next_digit) + curr[i+1:]
+
+                        if nxt not in deadends_set and nxt not in visited:
+                            q.append(nxt)
+                            visited.add(nxt)
+
+            steps += 1
+        
+        return -1
