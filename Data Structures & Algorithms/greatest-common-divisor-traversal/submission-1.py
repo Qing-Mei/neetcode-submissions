da99e5f308f5ea2d13eddb95d4bcb461eeff57c9
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-09-06 22:16:21 -0400

    Add: greatest-common-divisor-traversal - submission-1

---
+++ Data Structures & Algorithms/greatest-common-divisor-traversal/submission-1.py
@@ -0,0 +1,63 @@
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        if n == 1:
+            return True
+        
+        if 1 in nums:
+            return False
+        
+        max_num = max(nums)
+
+        spf = list(range(max_num + 1))
+
+        for prime in range(2, int(max_num ** 0.5) + 1):
+            if spf[prime] == prime:
+                for multiple in range(prime * prime, max_num + 1, prime):
+                    if spf[multiple] == multiple:
+                        spf[multiple] = prime
+        
+        parent = list(range(n))
+        size = [1] * n
+        components = n
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            nonlocal components
+
+            root_x = find(x)
+            root_y = find(y)
+
+            if root_x == root_y:
+                return
+            
+            if size[root_y] > size[root_x]:
+                root_x, root_y = root_y, root_x
+            
+            parent[root_y] = root_x
+            size[root_x] += size[root_y]
+            components -= 1
+        
+        prime_owner = {}
+
+        for i, val in enumerate(nums):
+            num = val
+
+            while num > 1:
+                prime = spf[num]
+
+                if prime in prime_owner:
+                    union(i, prime_owner[prime])
+                else:
+                    prime_owner[prime] = i
+
+                while num % prime == 0:
+                    num //= prime            
+        
+        return components == 1
+
