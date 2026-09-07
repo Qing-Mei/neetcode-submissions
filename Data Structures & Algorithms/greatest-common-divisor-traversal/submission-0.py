2577223d9ab16ed245a6bf50eff1da5d99a608e2
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-09-06 21:24:10 -0400

    Add: greatest-common-divisor-traversal - submission-0

---
+++ Data Structures & Algorithms/greatest-common-divisor-traversal/submission-0.py
@@ -0,0 +1,56 @@
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        if n == 1:
+            return True
+        
+        if 1 in nums:
+            return False
+        
+        parent = list(range(n))
+        size = [1] * n
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            root_x = find(x)
+            root_y = find(y)
+
+            if root_x == root_y:
+                return
+            
+            if size[root_y] > size[root_x]:
+                root_x, root_y = root_y, root_x
+            
+            parent[root_y] = root_x
+            size[root_x] += size[root_y]
+        
+        prime_owner = {}
+
+        for i, num in enumerate(nums):
+            factor = 2
+
+            while factor * factor <= num:
+                if num % factor == 0:
+                    if factor in prime_owner:
+                        union(i, prime_owner[factor])
+                    else:
+                        prime_owner[factor] = i
+                    
+                    while num % factor == 0:
+                        num //= factor
+                
+                factor += 1
+            
+            if num > 1:
+                if num in prime_owner:
+                    union(i, prime_owner[num])
+                else:
+                    prime_owner[num] = i
+        
+        root = find(0)
+        return all(find(i) == root for i in range(1, n))
