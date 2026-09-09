0dd046d8d02004d816fe05f5b4740b0482d4d71b
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-09-08 22:55:15 -0400

    Add: stone-game-iii - submission-7

---
+++ Data Structures & Algorithms/stone-game-iii/submission-7.py
@@ -0,0 +1,21 @@
+from functools import cache
+
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+        # dp[i] = dfs(i)
+        dp = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            dp[i] = float("-inf")
+            take = 0
+
+            for j in range(i, min(i + 3, n)):
+                take += stoneValue[j]
+                dp[i] = max(dp[i], take - dp[j + 1])
+        
+        if dp[0] > 0:
+            return "Alice"
+        if dp[0] < 0:
+            return "Bob"
+        return "Tie"
