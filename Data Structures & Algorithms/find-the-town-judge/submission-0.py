7c57e397db24a3656e93c3291a34da6353356f33
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-09-08 22:04:57 -0400

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        indegree = defaultdict(int)
+        outdegree = defaultdict(int)
+
+        for a, b in trust:
+            indegree[b] += 1
+            outdegree[a] += 1
+        
+        for i in range(1, n + 1):
+            if outdegree[i] == 0 and indegree[i] == n - 1:
+                return i
+        
+        return -1
