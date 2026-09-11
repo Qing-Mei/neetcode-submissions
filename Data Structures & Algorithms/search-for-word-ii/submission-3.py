20126400d2203cb1da7310aae807eb5b1d79cd13
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-09-10 22:10:25 -0400

    Add: search-for-word-ii - submission-3

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-3.py
@@ -0,0 +1,55 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = ""
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+
+        for word in words:
+            node = root
+
+            for ch in word:
+                if ch not in node.children:
+                    node.children[ch] = TrieNode()
+                node = node.children[ch]
+            
+            node.word = word
+
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        m = len(board)
+        n = len(board[0])
+        res = []
+        visited = set()
+
+        def dfs(i, j, node):
+            ch = board[i][j]
+
+            if ch not in node.children:
+                return False
+            
+            node = node.children[ch]
+
+            if node.word:
+                res.append(node.word)
+                node.word = ""
+                
+            visited.add((i, j))
+
+            for di, dj in dirs:
+                ni = i + di
+                nj = j + dj
+
+                if 0 <= ni < m and 0 <= nj < n and (ni, nj) not in visited:
+                    dfs(ni, nj, node)
+
+            visited.remove((i, j))
+            
+            return
+        
+        for i in range(m):
+            for j in range(n):
+                dfs(i, j, root)
+        
+        return res
