05ee6eec7053172a2e65ae3054b612fb86a962e6
Qing-Mei <116045280+Qing-Mei@users.noreply.github.com>
2026-09-18 23:21:24 -0400

    Add: dota2-senate - submission-1

---
+++ Data Structures & Algorithms/dota2-senate/submission-1.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        # RRDDDRRDRDR
+        # |  
+        # r: 0
+        # d: 0
+        q = deque(senate)
+
+        r_count= senate.count("R")
+        d_count = len(senate) - r_count
+
+        r, d = 0, 0
+
+        while r_count > 0 and d_count > 0:
+            curr = q.popleft()
+
+            if curr == "R":
+                if d > 0:
+                    d -= 1
+                    r_count -= 1
+                else:
+                    q.append(curr)
+                    r += 1
+            else:
+                if r > 0:
+                    r -= 1
+                    d_count -= 1
+                else:
+                    q.append(curr)
+                    d += 1
+        
+        return "Radiant" if r_count > 0 else "Dire"
+        
